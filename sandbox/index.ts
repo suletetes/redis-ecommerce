651ff243d4343595797e1b90b0e8d4b2a27c97cb
suleabdul <sulea841@gmail.com>
2025-01-02 17:11:51 +0100

    hashset test in the sandbox

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,15 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 2020,
+		engine: {cylinder: 8},
+		owner: null,
+		service: undefined
+	})
+
+	const car = await  client.hGetAll('car')
+};
 run();
