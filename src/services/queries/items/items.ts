8794727d7e9e234d8d6a80fc9cd5eedbd980d54e
suleabdul <sulea841@gmail.com>
2025-01-02 21:58:52 +0100

    items.ts

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -3,16 +3,29 @@ import { client } from '$services/redis';
 import { serialize } from '$services/queries/items/serialize';
 import { genId } from '$services/utils';
 import { itemsKey } from '$services/keys';
-import {deserialize} from '$services/queries/items/deserialize';
+import { deserialize } from '$services/queries/items/deserialize';
 
 export const getItem = async (id: string) => {
 	const item = await client.hGetAll(itemsKey(id));
 	if (Object.keys(item).length === 0) {
-		return null
+		return null;
 	}
+	return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemsKey(id));
+	});
+	const results = await Promise.all(commands);
+
+	return results.map((result, i) => {
+		if (Object.keys(result).length === 0) {
+			return null;
+		}
+		return deserialize(ids[i], result);
+	});
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
