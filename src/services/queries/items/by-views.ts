a8d1b02bfb779eae1bcc0685c8919bfd1d02eafe
suleabdul <sulea841@gmail.com>
2025-01-03 20:57:55 +0100

    by-views.ts

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,23 +1,32 @@
-import {client} from '$services/redis';
+import { client } from '$services/redis';
 import { itemsByViewsKey, itemsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) =>
-{
-	const results = await client.sort(
-		itemsByViewsKey(), {
-			GET: [
-				'#',
-				`${itemsKey('*')}->name`,
-				`${itemsKey('*')}->views`
-			],
-			BY: 'score',
-			DIRECTION: order,
-			LIMIT: {
-				offset,
-				count
-			}
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	let results: any = await client.sort(itemsByViewsKey(), {
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
+		BY: 'score',
+		DIRECTION: order,
+		LIMIT: {
+			offset,
+			count
 		}
-	)
+	});
 
+	const items = [];
 
+	while (items.length) {
+		const [id, name, views, endingAt, imageUrl, price,  ...rest] = results;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+		items.push(item);
+		results = rest;
+	}
+	return items
 };
