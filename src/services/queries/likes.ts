c7b95c734a12b690b9eeedfa718cb79e25925adf
suleabdul <sulea841@gmail.com>
2025-01-03 19:11:06 +0100

    likes.ts by-views.ts

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,20 +1,32 @@
 import { client } from '$services/redis';
-import { userLikesKey } from '$services/keys';
+import { itemsKey, userLikesKey } from '$services/keys';
+import {getItems} from '$services/queries/items';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
 	return client.sIsMember(userLikesKey(userId), itemId);
 };
 
 export const likedItems = async (userId: string) => {
+	const ids = await  client.sMembers(userLikesKey(userId))
 
+	return  getItems(ids)
 };
 
 export const likeItem = async (itemId: string, userId: string) => {
-	await client.sAdd(userLikesKey(userId), itemId);
+	const inserted = await client.sAdd(userLikesKey(userId), itemId);
+	if (inserted) {
+		return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+	}
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
-	await client.sRem(userLikesKey(userId), itemId);
+	const removed = await client.sRem(userLikesKey(userId), itemId);
+	if (removed) {
+		return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+	}
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+	const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)])
+	return getItems(ids)
+};
